09cbd5b66706eccb180adccf0ab7ed8c5438e115
XiaoLingH <xhuang0626@gmail.com>
2024-02-28 23:26:25 -0500

    create fields for users in user.java

---
+++ src/main/java/User.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private boolean enabled = true;
+    private String email;
+}
