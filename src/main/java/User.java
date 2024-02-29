557064683758329bd08e06083389a948280d3e56
XiaoLingH <xhuang0626@gmail.com>
2024-02-28 23:40:16 -0500

    add getter and setter function to user.java file

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -6,4 +6,28 @@ public class User {
     private String password;
     private boolean enabled = true;
     private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
