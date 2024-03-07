5e822c255ac9921449ce3da4917bad9eb0839e9f
XiaoLingH <xhuang0626@gmail.com>
2024-03-07 08:45:19 -0500

    fix WebMvcConfigurer interface not found

--- personnel-management/src/main/java/com/bcstudents/personnelmanagement/config/MvcConfig.java
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/config/MvcConfig.java
@@ -3,10 +3,11 @@ package com.bcstudents.personnelmanagement;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.context.annotation.WebMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// Extend the WebMvcConfigurer interface
 @Configuration
-public class MvcConfig implements WebMvcConfiguration{
+public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");
