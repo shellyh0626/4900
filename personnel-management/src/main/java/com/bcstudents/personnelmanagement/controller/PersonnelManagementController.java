3874128d79804be8e9a1f0e41cd9eb56a9bc9723
EonAugety <xinhuichen55@gmail.com>
2024-03-07 21:58:01 -0500

    login check

--- personnel-management/src/main/java/com/bcstudents/personnelmanagement/controller/PersonnelManagementController.java
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/controller/PersonnelManagementController.java
@@ -1,8 +1,11 @@
 package com.bcstudents.personnelmanagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.util.StringUtils;
 
 // Spring Boot allow auto configuration
 @RestController
@@ -10,7 +13,13 @@ public class PersonnelManagementController {
     // Invoke usage and receive parameters from frontend.
     // http://localhost:8080/hello
     @RequestMapping("/hello")
-    public String hello() {
+    public String hello(@RequestParam("username") String username, @RequestParam("password") String password, Model model)
+    {
+        if(!StringUtils.isEmpty(username) && "09876".equals(password)){
+            return "dassboard";
+        }else {
+
+        }
         return "hello";
     }
 }
