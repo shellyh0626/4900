5d79e3d95d2b1c9cea3005b9b25bf925f084c09e
XiaoLingH <xhuang0626@gmail.com>
2024-03-09 16:40:31 -0500

    import packages for mapping

--- personnel-management/src/main/java/com/bcstudents/personnelmanagement/controller/EmpController.java
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/controller/EmpController.java
@@ -1,10 +1,15 @@
 package com.bcstudents.personnelmanagement.controller;
 
+import com.bcstudents.personnelmanagement.dao.DepartmentDao;
 import com.bcstudents.personnelmanagement.dao.EmployeeDao;
+import com.bcstudents.personnelmanagement.pojo.Department;
 import com.bcstudents.personnelmanagement.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -21,16 +26,16 @@ public class EmpController {
         model.addAttribute("emps", employees);
         return "list";
     }
-    @GetMapping(“/emp/{id}”)
-    public string toUpdateEmp(@PathVariable(“id”)Integer id, Model model {
+    @GetMapping("/emp/{id}")
+    public String toUpdateEmp(@PathVariable("id")Integer id, Model model {
         Employee employee = employeeDao.getEmployeeById(id);
-        model.addAttribute(s:”emp”, employee);
-        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute(s:"emp", employee);
+        Collection<Department> departments = DepartmentDao.getDepartments();
         Model.addAttribute(s:"departments", departments);
-        return “emp/update”;
+        return "emp/update";
     }
     @PostMapping("/updateEmp")
-    public string updateEmp(Employee employee){
+    public String updateEmp(Employee employee){
         employeeDao.save(employee);
         return "redirect:/emps";
     }
