54382fc5c5be3a00e53d5ecfe017a100664260c1
EonAugety <xinhuichen55@gmail.com>
2024-03-09 14:14:04 -0500

    Addpage

--- personnel-management/src/main/java/com/bcstudents/personnelmanagement/controller/EmpController.java
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/controller/EmpController.java
@@ -1,10 +1,13 @@
 package com.bcstudents.personnelmanagement.controller;
 
+import com.bcstudents.personnelmanagement.dao.DepartmentDao;
 import com.bcstudents.personnelmanagement.dao.EmployeeDao;
+import com.bcstudents.personnelmanagement.pojo.Department;
 import com.bcstudents.personnelmanagement.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -14,11 +17,20 @@ public class EmpController {
 
     @Autowired
     EmployeeDao employeeDao;
-
+    @Autowired
+    DepartmentDao departmentDao;
     @RequestMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps", employees);
         return "list1";
     }
+    @GetMapping("/emp")
+    public String Addpage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emp/add";
+    }
 }
+
+
