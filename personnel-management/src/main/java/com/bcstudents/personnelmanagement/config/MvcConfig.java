b34cb8ad3329e872b4c13d73979216855cac3d3b
Mery <104753438+MeryCH9@users.noreply.github.com>
2024-03-09 18:38:35 -0500

    fix somes codes

--- personnel-management/src/main/java/com/bcstudents/personnelmanagement/config/MvcConfig.java
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/config/MvcConfig.java
@@ -2,11 +2,18 @@ package com.bcstudents.personnelmanagement.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // Extend the WebMvcConfigurer interface
 @Configuration
-@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboead1");
+    }
 
 }
--- personnel-management/src/main/java/com/bcstudents/personnelmanagement/controller/PersonnelManagementController.java
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/controller/PersonnelManagementController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.util.StringUtils;
 
@@ -12,14 +13,14 @@ import org.thymeleaf.util.StringUtils;
 public class PersonnelManagementController {
     // Invoke usage and receive parameters from frontend.
     // http://localhost:8080/hello
-    @RequestMapping("/hello")
-    public String hello(@RequestParam("username") String username, @RequestParam("password") String password, Model model)
+    @RequestMapping("/user/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model)
     {
         if(!StringUtils.isEmpty(username) && "09876".equals(password)){
-            return "dassboard";
+            return "redirect:/main.html";
         }else {
-
+            model.addAttribute("msg","Incorrect username or password!");
+            return "index";
         }
-        return "hello";
     }
 }
