27774b4909e8a91dc884b11cfda35d390841abee
XiaoLingH <xhuang0626@gmail.com>
2024-05-09 14:45:54 -0400

    update user files

---
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/bean/User.java
@@ -0,0 +1,54 @@
+package com.bcstudents.personnelmanagement.bean;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private boolean enabled = true;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    //  add enabled function, getter method
+    public boolean getEnabled() {
+        return enabled;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
