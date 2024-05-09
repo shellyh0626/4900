457716a5bff6a4939bd2feab369b7e82c0fc361a
rongxin2430 <rongxin233472430@gmail.com>
2024-05-09 12:46:27 -0400

    NationServiceImpl.java INationService.java

---
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/service/INationService.java
@@ -0,0 +1,9 @@
+package com.bcstudents.personnelmanagement.service;
+
+import com.bcstudents.personnelmanagement.Nation;
+
+import java.util.List;
+
+public interface INationService {
+    List<Nation> getAllNations();
+}
