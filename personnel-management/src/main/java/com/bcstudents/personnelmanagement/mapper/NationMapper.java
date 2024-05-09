eb337a4ca4deac1ca16b5e373e6880a31caf3777
rongxin2430 <rongxin233472430@gmail.com>
2024-05-08 22:24:09 -0400

    NationMapper.java NationMapper.xml

---
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/mapper/NationMapper.java
@@ -0,0 +1,9 @@
+package com.bcstudents.personnelmanagement.mapper;
+
+import com.bcstudents.personnelmanagement.Nation;
+
+import java.util.List;
+
+public interface NationMapper {
+    List<Nation> getAllNations();
+}
