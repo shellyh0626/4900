4ea838567cb6f341c4ee3a1b803af9446c135581
rongxin2430 <rongxin233472430@gmail.com>
2024-05-10 13:38:06 -0400

    PositionMapper.java Position.xml

---
+++ personnel-management/src/main/java/com/bcstudents/personnelmanagement/mapper/PositionMapper.java
@@ -0,0 +1,9 @@
+package com.bcstudents.personnelmanagement.mapper;
+
+import com.bcstudents.personnelmanagement.Position;
+
+import java.util.List;
+
+public interface PositionMapper {
+    List<Position> getAllPositions();
+}
